0c6cbd88319c8178f4bcebb6b5a733c5d5139167
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-10-10 21:15:14 +0200

    whitespaces

--- markdown-shortcode.php
+++ markdown-shortcode.php
@@ -11,77 +11,79 @@ add_action('plugins_loaded', array('Markdown_Shortcode_Plugin', 'get_instance'))
 
 class Markdown_Shortcode_Plugin {
 
-	private static $instance;
+  private static $instance;
 
-	public static function get_instance() {
-		if (!self::$instance) {
-			self::$instance = new self();
-		}
+  public static function get_instance() {
+    if (!self::$instance) {
+      self::$instance = new self();
+    }
 
-		return self::$instance;
-	}
+    return self::$instance;
+  }
 
-	function __construct() {
-		$this->cache = array();
-		add_action('init', array(&$this, 'init'));
-	}
+  function __construct() {
+    $this->cache = array();
+    add_action('init', array(&$this, 'init'));
+  }
 
-	function init() {
-		add_action('wp_enqueue_scripts', array(&$this, 'init_highlight'));
-		add_shortcode('markdown', array(&$this, 'markdown_shortcode'));
-		add_filter('the_content', array(&$this, 'markdown_shortcode_preprocess'), 1);
-	}
+  function init() {
+    add_action('wp_enqueue_scripts', array(&$this, 'init_highlight'));
+    add_shortcode('markdown', array(&$this, 'markdown_shortcode'));
+    add_filter('the_content', array(&$this, 'markdown_shortcode_preprocess'), 1);
+  }
 
-	function init_highlight() {
-		wp_enqueue_style("highlight",  plugins_url('highlight/styles/github.css', __FILE__));
-		wp_enqueue_script("highlight",  plugins_url('highlight/highlight.min.js', __FILE__));
-		wp_enqueue_script("highlight_init",  plugins_url('init_highlight.js', __FILE__));
-	}
+  function init_highlight() {
+    wp_enqueue_style("highlight", plugins_url('highlight/styles/github.css', __FILE__));
+    wp_enqueue_script("highlight", plugins_url('highlight/highlight.min.js', __FILE__));
+    wp_enqueue_script("highlight_init", plugins_url('init_highlight.js', __FILE__));
+  }
 
-	function markdown_shortcode($attr, $content = null) {
-		require_once('parsedown/Parsedown.php');
-		require_once('parsedown/ParsedownExtra.php');
+  function markdown_shortcode($attr, $content = null) {
+    require_once('parsedown/Parsedown.php');
+    require_once('parsedown/ParsedownExtra.php');
 
-		if (isset($this->cache[$content])) $content = $this->cache[$content];
+    if (isset($this->cache[$content])) {
+      $content = $this->cache[$content];
+    }
 
-		$content = html_entity_decode($content);
-		$content = trim($content);
-		$content = $this->underscores_to_spaces($content);
+    $content = html_entity_decode($content);
+    $content = trim($content);
+    $content = $this->underscores_to_spaces($content);
 
-		$extra = new markdown_shortcode\ParsedownExtra();
-		$parsed_content = $extra->text($content);
+    $extra = new markdown_shortcode\ParsedownExtra();
+    $parsed_content = $extra->text($content);
 
-		return $parsed_content;
-	}
+    return $parsed_content;
+  }
 
-	function markdown_shortcode_pre($attr, $content = null) {
-		$key = sha1($content);
-		$this->cache[$key] = $content;
-		return "[markdown]{$key}[/markdown]";
-	}
+  function markdown_shortcode_pre($attr, $content = null) {
+    $key = sha1($content);
+    $this->cache[$key] = $content;
+    return "[markdown]{$key}[/markdown]";
+  }
 
-	function markdown_shortcode_preprocess($content) {
-		global $shortcode_tags;
+  function markdown_shortcode_preprocess($content) {
+    global $shortcode_tags;
 
-		// Backup current registered shortcodes and clear them all out
-		$orig_shortcode_tags = $shortcode_tags;
-		$shortcode_tags = array();
+    // Backup current registered shortcodes and clear them all out
+    $orig_shortcode_tags = $shortcode_tags;
+    $shortcode_tags = array();
 
-		add_shortcode('markdown', array(&$this, 'markdown_shortcode_pre'));
+    add_shortcode('markdown', array(&$this, 'markdown_shortcode_pre'));
 
-		// Do the shortcode (only the one above is registered)
-		$content = do_shortcode($content);
+    // Do the shortcode (only the one above is registered)
+    $content = do_shortcode($content);
 
-		// Put the original shortcodes back
-		$shortcode_tags = $orig_shortcode_tags;
-		return $content;
-	}
+    // Put the original shortcodes back
+    $shortcode_tags = $orig_shortcode_tags;
+    return $content;
+  }
 
-	// Replaces more than one underscore to same amount of spaces
-	function underscores_to_spaces($content) {
-	    $content = preg_replace_callback('/_{2,}/', function ($matches) {
-	        return str_replace('_', ' ', $matches[0]);
-	    }, $content);
-	    return $content;
-	}
+  // Replaces more than one underscore to same amount of spaces
+  function underscores_to_spaces($content) {
+      $content = preg_replace_callback('/_{2,}/', function ($matches) {
+          return str_replace('_', ' ', $matches[0]);
+      }, $content);
+      return $content;
+  }
 }
